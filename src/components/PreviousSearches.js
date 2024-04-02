cdf44d3c2ea8214b4b3a3a0e4ed6c166d8940508
Samuel de Carvalho <samuel@guardianrh.com.br>
2024-04-02 00:33:44 -0300

    Retirando a função de pesquisas anteriores e ajustando posições de campos

--- src/components/PreviousSearches.js
+++ src/components/PreviousSearches.js
@@ -2,24 +2,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function PreviousSearches() {
-    const searches = ['bolo 1', 'doce 1', 'recheio 1', 'panetone 1']
-
 
     return (
         <div className="previous-searches section">
-            <h2 className="h2">Previous Searches</h2>
-            <div className="previous-searches-container">
-                {searches.map(search => (<div className="search-item">
-                    {search}
-                </div>))}
-            </div>
             <div className="search-box">
-                <input type="text" placeholder="search ..." className="input" />
+                <input type="text" placeholder="Search..." className="input" />
                 <button className="btn">
                     <FontAwesomeIcon icon={faSearch} />
                 </button>
             </div>
         </div>
     )
-
 }
